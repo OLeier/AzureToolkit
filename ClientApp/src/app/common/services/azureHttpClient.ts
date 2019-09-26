1bbb7e03ddb3b5fa6b843e135f2d6cfbf2352efa
O.Leier <git.OLr@ib-leier.net>
2019-09-27 01:18:39 +0200

    Module 2 | Cognitive Services Tutorial Lab01: Implementing Bing Search Services To Expose Search Services - is running
    - '@angular/http'; is @deprecated use @angular/common/http instead
    - HttpHeaders is immutable.
    - =${searchTerm} not working
    - bing search v5.0 -> v7.0

--- ClientApp/src/app/common/services/azureHttpClient.ts
+++ ClientApp/src/app/common/services/azureHttpClient.ts
@@ -1,21 +1,24 @@
-import {Injectable} from '@angular/core';
- import {Http, Headers} from '@angular/http';
- @Injectable()
- export class AzureHttpClient {
-     constructor(private http: Http) {}
-     get(url: string, apiKey: string) {
-         let headers = new Headers();
-         headers.append('Ocp-Apim-Subscription-Key', apiKey);
-         return this.http.get(url, {
-             headers: headers
-         });
-     }
-     post(url, apiKey, data) {
-         let headers = new Headers();
-         headers.append('Ocp-Apim-Subscription-Key', apiKey);
-         return this.http.post(url, data, {
-             headers: headers
-         });
-     }
- }
- 
+import { Injectable } from '@angular/core';
+//import { Http, Headers } from '@angular/http';  // @deprecated use @angular/common/http instead
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Injectable()
+export class AzureHttpClient {
+
+    constructor(private http: HttpClient) { }
+
+    get(url: string, apiKey: string) {
+        let headers = new HttpHeaders();
+        headers = headers.append('Ocp-Apim-Subscription-Key', apiKey);
+        return this.http.get(url, {
+            headers: headers
+        });
+    }
+
+    post(url: string, apiKey: string, data: any) {
+        let headers = new HttpHeaders();
+        headers = headers.append('Ocp-Apim-Subscription-Key', apiKey);
+        return this.http.post(url, data, {
+            headers: headers
+        });
+    }
+}
--- ClientApp/src/app/common/services/cognitive.service.ts
+++ ClientApp/src/app/common/services/cognitive.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+//import { Headers, Http } from '@angular/http';  // all imports are unused
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
@@ -8,10 +8,14 @@ import { BingSearchResponse } from '../models/bingSearchResponse';
 @Injectable()
 export class CognitiveService {
     bingSearchAPIKey = '66a85025246645d388d6fe56b6e72b43';  // TODO: config bingSearchAPIKey
+
     constructor(private http: AzureHttpClient) { }
+
     searchImages(searchTerm: string): Observable<BingSearchResponse> {
-        return this.http.get('https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=${searchTerm}', this.bingSearchAPIKey)
-            .map(response => response.json() as BingSearchResponse)
+        //let url = 'https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=${searchTerm}';
+        let url = 'https://imagesearch3.cognitiveservices.azure.com/bing/v7.0/images/search?q=' + searchTerm;
+        return this.http.get(url, this.bingSearchAPIKey)
+            .map(response => response as BingSearchResponse)
             .catch(this.handleError);
     }
     private handleError(error: any): Promise<any> {
